224bc811aff723690efa4f9016882f9d55cc95ec
Elias Ondrej <info@eliasondrej.com>
2023-02-13 14:58:57 +0100

    init

--- application/.prettierrc.cjs
+++ application/.prettierrc.cjs
@@ -1,4 +1,12 @@
 module.exports = {
     // ...require('@media-factory-cz/prettier-config'),
     ...require('@media-factory-cz/prettier-config'),
+    overrides: [
+        {
+            files: '*.json',
+            options: {
+                tabWidth: 2,
+            },
+        },
+    ],
 };
