acccc692d3b7186f41c487cf29d0d36d7fb417f3
Elias Ondrej <info@eliasondrej.com>
2023-02-27 14:06:52 +0100

    fixing vite build

--- application/packages/project-ui/vite.config.ts
+++ application/packages/project-ui/vite.config.ts
@@ -5,10 +5,26 @@ import { resolve } from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), dts({
-    insertTypesEntry: true,
-}),],
-  build: {
-    lib: {
-      entry: resolve(__dirname, 'src/index.ts'),
-})
+    plugins: [
+        react(),
+        dts({
+            insertTypesEntry: true,
+        }),
+    ],
+    build: {
+        lib: {
+            entry: resolve(__dirname, 'src/index.ts'),
+            name: '@mf-project/ui',
+            formats: ['es', 'umd', 'cjs'],
+        },
+        rollupOptions: {
+            external: ['react', 'react-dom'],
+            output: {
+                globals: {
+                    react: 'React',
+                    'react-dom': 'ReactDOM',
+                },
+            },
+        },
+    },
+});
