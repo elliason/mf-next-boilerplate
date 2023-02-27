ccb601abcf4561e7a7ac90d941667d82d8aa9535
Elias Ondrej <info@eliasondrej.com>
2023-02-27 15:30:46 +0100

    project utils package

---
+++ application/packages/project-utils/.eslintrc.cjs
@@ -0,0 +1,4 @@
+module.exports = {
+    root: true,
+    extends: ['@mf-project/eslint-config'],
+};
---
+++ application/packages/project-utils/src/index.ts
@@ -0,0 +1,6 @@
+export * from './errors/errors';
+
+// test
+export const test = () => {
+    console.log('test');
+};
