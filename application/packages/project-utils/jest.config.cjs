5be1685b3b94f98224a2d51cc91755e2792b6a63
Elias Ondrej <info@eliasondrej.com>
2023-02-28 07:43:12 +0100

    add jest to utils package

---
+++ application/packages/project-utils/jest.config.cjs
@@ -0,0 +1,12 @@
+const { compilerOptions } = require('./tsconfig.json');
+const { pathsToModuleNameMapper } = require('ts-jest');
+
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports = {
+    preset: 'ts-jest',
+    // testEnvironment: 'node',
+    testEnvironment: 'jest-environment-jsdom',
+    moduleNameMapper: pathsToModuleNameMapper(compilerOptions.paths, { prefix: '<rootDir>/' }),
+    collectCoverage: true,
+    verbose: true,
+};
